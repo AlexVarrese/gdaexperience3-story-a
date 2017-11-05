f1a88a81bb2c2b738ea8eabe5caf04478c8132f6
Tushar Dhote <tushar.dhote@rsinngp.rgen.com>
2017-11-05 13:48:24 +0530

    first commit

---
+++ src/ContosoAir.Clients/DataServices/SoloServiceProviders/ISoloServiceProvider.cs
@@ -0,0 +1,14 @@
+﻿
+using ContosoAir.Clients.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContosoAir.Clients.DataServices.SoloServiceProviders
+{
+    public interface ISoloServiceProvider
+    {
+        Task<IEnumerable<SoloService>> GetSoloServiceProviderAsync();
+
+        Task<string> GetReviewScore(List<Reviews> Reviews);
+    }
+}
